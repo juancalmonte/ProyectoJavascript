28ca6d1ab0bab9395fc1d68f13bb166cdb3d4d84
juancalmonte <33805822+juancalmonte@users.noreply.github.com>
2017-11-28 20:46:36 -0400

    carpeta recursos

---
+++ resources/js/estudiante.js
@@ -0,0 +1,80 @@
+
+                    var Estudiante = function()
+                    {
+                        var self = this;
+                        self.id = "";
+                        self.nombre = "";
+                        self.matricula = "";
+                        self.identificacion = "";
+                        self.telefono = "";
+                        self.email = "";
+                    }
+        
+                    function recorrer()
+                    {
+                    var id = document.getElementById("id").value;
+                    
+                    var nombre = document.getElementById("nombre").value;
+                    
+                    var matricula = document.getElementById("matricula").value;
+                
+                    var identificacion = document.getElementById("identificacion").value;
+            
+                    var telefono = document.getElementById("telefono").value;
+                    
+                    var email = document.getElementById("email").value;
+                      
+                     var est1 = new Estudiante();
+                     est1.id = id;
+                     est1.nombre = nombre;
+                     est1.matricula = matricula;
+                     est1.identificacion = identificacion;
+                     est1.telefono = telefono;
+                     est1.email = email;
+                     console.log(" impresion de la clase  " );
+                     console.log(est1);
+        
+                    addRow(est1);                
+        
+                    };
+                    function addRow(est1)
+                    {
+                        var table = document.getElementById("estudiantes");
+                        var tr = document.createElement("tr");
+                        var tdId = document.createElement("td");
+                        var tdNombre = document.createElement("td");
+                        var tdMatricula = document.createElement("td");
+                        var tdIdentificacion = document.createElement("td");
+                        var tdTelefono = document.createElement("td");
+                        var tdemail = document.createElement("td");
+        
+        
+                        var txtId = document.createTextNode(est1.id);
+                        var txtNombre = document.createTextNode(est1.nombre);
+                        var txtMatricula = document.createTextNode(est1.matricula);
+                        var txtIdentificacion = document.createTextNode(est1.identificacion);
+                        var txtTelefono = document.createTextNode(est1.telefono);
+                        var txtemail = document.createTextNode(est1.email);
+        
+                        tdId.appendChild(txtId);
+                        tr.appendChild(tdId);
+        
+                        tdNombre.appendChild(txtNombre);
+                        tr.appendChild(txtNombre);
+        
+                        tdMatricula.appendChild(txtMatricula);
+                        tr.appendChild(txtMatricula);
+        
+                        
+                        tdMatricula.appendChild(txtIdentificacion);
+                        tr.appendChild(txtIdentificacion);
+        
+                        tdMatricula.appendChild(txtTelefono);
+                        tr.appendChild(txtTelefono);
+        
+                        tdMatricula.appendChild(txtemail);
+                        tr.appendChild(txtemail);
+        
+                        table.appendChild(tr);
+                       
+                    }
